80b784467d15977aad15ebc93fd02d27440f0539
adefirmanf <syskeys32@gmail.com>
2018-07-14 00:49:25 +0700

    Change _address variable to msg.sender

--- contracts/PixivRanking.sol
+++ contracts/PixivRanking.sol
@@ -26,47 +26,50 @@ contract PixivRanking{
             string imageName;
         }   
         
-        mapping (address => Illustrator) illustrator;
-        mapping (uint256 => Image) images;
+        mapping (address => Illustrator) public illustrator;
+        mapping (uint256 => Image) public images;
         
         address[] public IllustratorAccs;
         
-        
-        function AddIllustrator (address _address, uint _IllustratorID, string _name) public {
-            var illust = illustrator[_address];
+        //Creating an account
+        function AddIllustrator (uint _IllustratorID, string _name) public  {
+            var illust = illustrator[msg.sender];
             illust.IllustratorID = _IllustratorID;
             illust.name = _name;
-            IllustratorAccs.push(_address) -1;
+            IllustratorAccs.push(msg.sender) -1;
         }
         
         function GetArray() public view returns(address[]){
             return IllustratorAccs;
         }
         
-        function AddImage (address _IllustratorImage, uint256 _imageID, string _nameImage) public {
+        function AddImage (uint256 _imageID, string _nameImage) public {
             var image = images[_imageID];
             image.ImageID = _imageID;
             image.imageName = _nameImage;
-            illustrator[_IllustratorImage].images.push(_imageID) -1;
+            illustrator[msg.sender].images.push(_imageID) -1;
         }
         
-        function GetImage(address _IllustratorImage) public returns (uint[]){
-            return illustrator[_IllustratorImage].images;
+        function GetImage() public view returns (uint256[]){
+            return illustrator[msg.sender].images;
         }
         
-        function IndividualImage(address _IllustratorImage) public returns (bool){
-            
+        function IndividualImage(address _IllustratorImage) public view returns (bool){
             return false;
         }
         
-        function AddBookmark(address _IllustratorImage, uint256 _imageID) public {
-            var IllustImagelength = illustrator[_IllustratorImage].images.length;
+        function AddBookmark(uint256 _imageID) public {
+            var IllustImagelength = illustrator[msg.sender].images.length;
             for(uint i = 0; i < IllustImagelength; i++){
-                    if(illustrator[_IllustratorImage].images[i] == _imageID){
-                    throw;  
+                    if(illustrator[msg.sender].images[i] == _imageID){
+                    throw;  //ERROR cause owner can't bookmarked his image.
                     } 
             }
             images[_imageID].Bookmarkscount = 1;    
+        }
+
+        function CheckSender() public view returns (address){
+            return msg.sender;
         }
 
 }
