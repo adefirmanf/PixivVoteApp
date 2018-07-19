da9fc1ad0ece62ba86f31645a56bb4ac7724ebaa
adefirmanf <syskeys32@gmail.com>
2018-07-19 16:29:22 +0700

    Refer to Issue #2 : Add Web3.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
     <div id="body">
-        HELLO FROM VUE.JS
+        HELLO FROM VUE.JS</br>
+        <small>Everything was done. Creator and founder of {{name}} </small>
     </div>
 </template>
+<script>
+import Web3 from 'web3'
+export default {
+    data(){
+        return {
+            'name' : "Ade Firman F"
+        }
+    },
+    mounted(){
+        let web3 = new Web3()
+        console.log(web3)
+    }
+}
+</script>
