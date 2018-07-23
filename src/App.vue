21dfb26ff46e0741cf30e6ea3ae6b9354e38428f
adefirmanf <syskeys32@gmail.com>
2018-07-23 16:55:52 +0700

    Add login page

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,8 @@ export default {
     }
 }
 </script>
+<style>
+    *{
+        font-family : 'Noto Sans', sans-serif;
+    }
+</style>
--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,5 +1,64 @@
 <template>
     <div id="body">
-        HELLO WORLD
+        <div class="centered">
+            <el-card :body-style="{ padding: '20px' }" shadow="never">
+                    <div class="logo">
+                        <img src="./pixiv.svg" style="width : 60%"/>
+                        <div class="logo-description">
+                            <small>Spice up your creating proccess</small>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="button-group">
+                        <el-button type="primary" class="button-custom" size="small">Login with pixiv account</el-button></br>
+                        <el-button type="info" class="button-custom" size="small">No, Just explore</el-button>
+                    </div>
+            </el-card>
+        </div>
+
     </div>
 </template>
+<script>
+import Web3 from 'web3'
+export default {
+    data(){
+        return {
+            'data' : '',
+            'url' : "/@/assets/img/pixiv.svg"
+        }
+    },
+}
+</script>
+<style>
+    body{
+    
+    }
+    .centered{
+        margin : 20vh auto 0 auto;
+        width : 363px;
+        text-align : center;
+    }
+    .logo > .logo-description{
+        color : gray;
+        font-weight : bold;
+
+    }
+    .button-custom{
+        /*
+        padding-top : 5px;
+        padding-bottom : 5px;
+        */
+        margin-top : 5px;
+        width : 70%;
+    }
+    .button-group{
+        box-sizing : border-box;
+    }
+    /*
+        Margin given to 0 for disable horizontal scroll. Seems ElementUI giving margin when there any offsets
+    */
+    .centered > .el-row{
+        margin-left : 0 !important;
+        margin-right : 0 !important;
+    }
+</style>
