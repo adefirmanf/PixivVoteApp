37966a5d1759950b41ea66a681c65782e4df72a5
adefirmanf <syskeys32@gmail.com>
2018-07-27 15:22:32 +0700

    Modifyied login page

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,25 +1,40 @@
 <template>
-    <div id="body">
-        <div class="centered">
-            <el-card :body-style="{ padding: '20px' }" shadow="never">
-                    <div class="logo">
-                        <img src="./pixiv.svg" style="width : 60%"/>
-                        <div class="logo-description">
-                            <small>Spice up your creating proccess</small>
+    <div class="background">
+        <div id="body">
+            <div class="centered">
+                <el-card :body-style="{ padding: '20px' }" shadow="never">
+                        <div class="logo">
+                            <img src="./pixiv.svg" style="width : 60%"/>
+                            <img src="./blockchain.svg" style="width : 30%"/>
+                            <div class="logo-description">
+                                <small>Spread your illustrations and earn a money</small>
+                            </div>
                         </div>
-                    </div>
-                    <br>
-                    <div class="button-group">
-                        <el-button type="primary" class="button-custom" size="small">Login with pixiv account</el-button></br>
-                        <el-button type="info" class="button-custom" size="small">No, Just explore</el-button>
-                    </div>
-            </el-card>
+                        <br>
+                        <div class="button-group">
+                            <el-button type="primary" class="button-custom" size="small" @click="LoginPixiv">Login with pixiv account</el-button></br>
+                            <el-button type="info" class="button-custom" size="small">No, Just explore</el-button>
+                        </div>
+                        <div class="metamask bottom clearfix">
+                            <b>Ethereum account : <br></b>                                      
+                            <small>
+                            9x9z00z0x999120102919zkxjckzxjc0
+                            </small>
+                        </div>
+                </el-card>
+            </div>
         </div>
-
+            <div class="credit">
+                <div class="link">
+                居酒屋街 </br> <small>
+                Hourglass/さざなみ</small>
+                </div>
+            </div>
     </div>
 </template>
 <script>
 import Web3 from 'web3'
+import pixivAPI from 'pixiv-app-api'
 export default {
     data(){
         return {
@@ -27,11 +42,32 @@ export default {
             'url' : "/@/assets/img/pixiv.svg"
         }
     },
+    methods : {
+        LoginPixiv : function(){
+            
+        }
+    },
+    created : function() {
+        const pixiv = new pixivAPI()
+        pixiv.searchIllust('艦これ10000users入り').then((response)=>{
+            console.log(response)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
 }
 </script>
 <style>
     body{
-    
+        margin : 0px;
+    }
+    .metamask{
+        color : gray;
+        width : 100%;
+        text-align : center;
+        font-size : 14px;
+        margin-top : 10px;
+        margin-bottom : 10px;
     }
     .centered{
         margin : 20vh auto 0 auto;
@@ -44,21 +80,40 @@ export default {
 
     }
     .button-custom{
-        /*
-        padding-top : 5px;
-        padding-bottom : 5px;
-        */
         margin-top : 5px;
         width : 70%;
     }
     .button-group{
         box-sizing : border-box;
     }
-    /*
-        Margin given to 0 for disable horizontal scroll. Seems ElementUI giving margin when there any offsets
-    */
     .centered > .el-row{
         margin-left : 0 !important;
         margin-right : 0 !important;
+    }
+    .credit{
+        border-radius : 5px;
+        bottom: 0;
+        width: 180px;
+        position: absolute;
+        padding : 10px;
+        font-size : 14px;
+        background: rgba(0, 0, 0, 0.61);
+        margin-bottom: 30px;
+        z-index : -1;
+        margin-left : 10px;
+        content : '';
+    }
+    .link{
+        color : white;
+        opacity : 0.8;
+    }
+    .background{
+        width : 100%;
+        height : 100%;
+        position : fixed;
+        background-size : cover;
+        background-position : right top;
+        background-image : url('./sample.jpg');
+        background-repeat : no-repeat;
     }
 </style>
